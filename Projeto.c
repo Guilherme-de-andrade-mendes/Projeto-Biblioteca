731230cf48c7d62a7116369a9958144f995ac824
GuiAMendes <114957916+GuiAMendes@users.noreply.github.com>
2023-10-23 11:23:01 -0300

    Criação das funções do usuario parte - 1.

--- Projeto.c
+++ Projeto.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <locale.h>
 
+typedef struct{
+    int cpf;
+    char nome[100];
+    char rua[100];
+    char cep[9];
+    char telefones[100][11];
+    char emails[100][10];
+    char nascimento[11];
+    char profissao[100];
+} User;
+
 void Linhas(){
     printf("==========================================================================================\n");
 }
@@ -29,31 +40,111 @@ int SubmenuRelatorios() {
     return op;
 }
 
-int main() {
-    int opcao_Menu;
-    int opcao_Submenu;
-    int opcao_Relatorio;
-    setlocale(LC_ALL, "portuguese");
-    do {
-        opcao_Menu = MenuPrincipal();
-        if (opcao_Menu == 5)
+void IncluirUsuario(User usuario[], int indUser){
+    printf("====================== Incluindo Usuário ======================\nCPF: ");
+    scanf("%d", &usuario[indUser].cpf);
+    printf("Nome: ");
+    scanf("%s", usuario[indUser].nome);
+    printf("Data de nascimento (dia/mês/ano): ");
+    scanf("%s", usuario[indUser].nascimento);
+    printf("Rua: ");
+    scanf("%s", usuario[indUser].rua);
+    printf("CEP (xxxxx-xxx): ");
+    scanf("%s", usuario[indUser].cep);
+    int numTelefones = 0;
+    while (1){
+        printf("Telefone (xxxxx-xxxx): ");
+        scanf("%s", usuario[indUser].telefones[numTelefones]);
+        numTelefones++;
+        char resp;
+        printf("Deseja inserir mais um telefone (s/n)?: ");
+        scanf("%s", &resp);
+        if (resp == 'n')
             break;
-        switch (opcao_Menu) {
+    }
+    int numEmails = 0;
+    while (1){
+        printf("Email: ");
+        scanf("%s", usuario[indUser].telefones[numEmails]);
+        numEmails++;
+        char resp;
+        printf("Deseja inserir mais um email (s/n)?: ");
+        scanf("%s", &resp);
+        if (resp == 'n')
+            break;
+    }
+    printf("Profisão: ");
+    scanf("%s", usuario[indUser].profissao);
+}
+
+void imprimirDadosDoUsuario(User usuario[], int qntUserAtual){
+    for (int i = 0; i < qntUserAtual; i++){
+        printf("====================== Listar todos os usuários ======================\nUsuario %d* | Cpf: %d | Nome: %s | Rua: %s | CEP: %s | ", (i+1), usuario[i].cpf, usuario[i].nome,usuario[i].rua, usuario[i].cep);
+        printf("Telefone(s): [ ");
+        for(int j = 0; j < 11; j++)
+            printf("%s ", usuario[i].telefones[j]);
+        printf("] | Email(s): [ ");
+        for(int j = 0; j < 10; j++)
+            printf("%s ", usuario[i].emails[j]);
+        printf("] | Profissão: %s", usuario[i].profissao);
+    }
+}
+
+int buscarUsuario(User usuario[], int qntUserAtual){
+    int cpf;
+    printf("Digite o CPF do usuário desejado: ");
+    scanf("%d", &cpf);
+    for (int i = 0; i < qntUserAtual; i++){
+        if (usuario[i].cpf == cpf)
+            return i;
+    }
+    printf("O CPF digitado não consta na bases de dados atual. Tente novamente com um CPF válido.");
+    return -1;
+}
+
+void imprimirUsuarioEspecifico(User usuario[], int qntUserAtual){
+    int indUser = buscarUsuario(usuario, qntUserAtual);
+    if (indUser >= 0){
+        printf("====================== Listar todos os usuários ======================\nUsuario %d* | Cpf: %d | Nome: %s | Rua: %s | CEP: %s | ", (indUser+1), usuario[indUser].cpf, usuario[indUser].nome,usuario[indUser].rua, usuario[indUser].cep);
+        printf("Telefone(s): [ ");
+        for(int j = 0; j < 11; j++)
+            printf("%s ", usuario[indUser].telefones[j]);
+
+        printf("] | Email(s): [ ");
+        for(int j = 0; j < 10; j++)
+            printf("%s ", usuario[indUser].emails[j]);
+        printf("] | Profissão: %s", usuario[indUser].profissao);
+    }
+    else
+        printf("O usuário indicado não pode ser encontrado. Verifique se o mesmo consta na base de dados atual.");
+}
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+    User usuarios[255] = {0};
+    int opMenu, opSubMenu, opRelat, qntUser = 0;
+    do {
+        opMenu = MenuPrincipal();
+        if (opMenu == 5)
+            break;
+        switch (opMenu) {
             case 1:
                 printf("Submenu de Usuários\n");
                 while (1) {
-                    opcao_Submenu = Submenus();
-                    if (opcao_Submenu == 1)
-                        printf("Incluindo usuário.\n");
-                    else if (opcao_Submenu == 2)
-                        printf("Listar todos os usuários.\n");
-                    else if (opcao_Submenu == 3)
-                        printf("Listar um usuário específico.\n");
-                    else if (opcao_Submenu == 4)
+                    opSubMenu = Submenus();
+                    if (opSubMenu == 1){
+                        IncluirUsuario(usuarios, qntUser);
+                        qntUser++;
+                    }
+                    else if (opSubMenu == 2)
+                        imprimirDadosDoUsuario(usuarios, qntUser);                        
+                    else if (opSubMenu == 3)
+                        imprimirUsuarioEspecifico(usuarios, qntUser);
+                    else if (opSubMenu == 4)
                         printf("Alterando dados do usuário.\n");
-                    else if (opcao_Submenu == 5)
+                    else if (opSubMenu == 5)
                         printf("Excluindo usuário.\n");
-                    else if (opcao_Submenu == 6)
+                    else if (opSubMenu == 6)
                         break;
                     else
                         printf("Opção inválida. Entre com um número de submenu existente.\n");
@@ -62,18 +153,18 @@ int main() {
             case 2:
                 printf("Submenu de Livros\n");
                 while (1) {
-                    opcao_Submenu = Submenus();
-                    if (opcao_Submenu == 1)
+                    opSubMenu = Submenus();
+                    if (opSubMenu == 1)
                         printf("Incluindo livro.\n");
-                    else if (opcao_Submenu == 2)
+                    else if (opSubMenu == 2)
                         printf("Listar todos os livros.\n");
-                    else if (opcao_Submenu == 3)
+                    else if (opSubMenu == 3)
                         printf("Listar um livro específico.\n");
-                    else if (opcao_Submenu == 4)
+                    else if (opSubMenu == 4)
                         printf("Alterando dados de um livro.\n");
-                    else if (opcao_Submenu == 5)
+                    else if (opSubMenu == 5)
                         printf("Excluindo livro.\n");
-                    else if (opcao_Submenu == 6)
+                    else if (opSubMenu == 6)
                         break;
                     else
                         printf("Opção inválida. Entre com um número de submenu existente.\n");
@@ -82,18 +173,18 @@ int main() {
             case 3:
                 printf("Submenu de Empréstimos\n");
                 while (1) {
-                    opcao_Submenu = Submenus();
-                    if (opcao_Submenu == 1)
+                    opSubMenu = Submenus();
+                    if (opSubMenu == 1)
                         printf("Incluindo empréstimo.\n");
-                    else if (opcao_Submenu == 2)
+                    else if (opSubMenu == 2)
                         printf("Listar todos os empréstimos.\n");
-                    else if (opcao_Submenu == 3)
+                    else if (opSubMenu == 3)
                         printf("Listar um empréstimo específico.\n");
-                    else if (opcao_Submenu == 4)
+                    else if (opSubMenu == 4)
                         printf("Alterando dados de um empréstimo.\n");
-                    else if (opcao_Submenu == 5)
+                    else if (opSubMenu == 5)
                         printf("Excluindo empréstimo.\n");
-                    else if (opcao_Submenu == 6)
+                    else if (opSubMenu == 6)
                         break;
                     else
                         printf("Opção inválida. Entre com um número de submenu existente.\n");
@@ -102,14 +193,14 @@ int main() {
             case 4:
                 printf("Submenu de Relatório\n");
                 while (1) {
-                    opcao_Relatorio = SubmenuRelatorios();
-                    if (opcao_Relatorio == 1)
+                    opRelat = SubmenuRelatorios();
+                    if (opRelat == 1)
                         printf("Mostrar usuários por idade.\n");
-                    else if (opcao_Relatorio == 2)
+                    else if (opRelat == 2)
                         printf("Mostrar livros por autores.\n");
-                    else if (opcao_Relatorio == 3)
+                    else if (opRelat == 3)
                         printf("Mostrar dados por empréstimo.\n");
-                    else if (opcao_Relatorio == 4)
+                    else if (opRelat == 4)
                         break;
                     else
                         printf("Opção inválida. Entre com um número de submenu existente.\n");
@@ -119,7 +210,7 @@ int main() {
                 printf("Opção inválida. Entre com um número de submenu existente.\n");
                 break;
         }
-    } while (opcao_Menu != 5);
+    } while (opMenu != 5);
     Linhas();
     printf("Obrigado por utilizar nosso sistema. Volte sempre!\n");
     return 0;
